6dc5ca2309d73dbd750fe230e266f4b14210a586
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-04-08 17:07:25 -0400

    set up hash routes

--- index.js
+++ index.js
@@ -4,19 +4,40 @@ function WebmailViewModel() {
   self.folders = ["Inbox", "Archive", "Sent", "Spam"];
   self.chosenFolderId = ko.observable();
   self.chosenFolderData = ko.observable();
+  self.chosenMailData = ko.observable();
 
   // Behaviours
   self.goToFolder = folder => {
-    self.chosenFolderId(folder);
-    $.get(
-      "http://learn.knockoutjs.com/mail",
-      { folder: folder },
-      self.chosenFolderData
-    );
+    /*self.chosenFolderId(folder);
+    $.get("/mail", { folder: folder }, self.chosenFolderData);*/
+    location.hash = folder;
   };
 
-  // Show inbox by default
-  self.goToFolder("Inbox");
+  self.goToMail = mail => {
+    /*self.chosenFolderId(mail.folder);
+    self.chosenFolderData(null);
+    $.get("/mail", {mailId: mail.id}, self.chosenMailData);*/
+    location.hash = mail.folder + "/" + mail.id;
+  };
+
+  // Client side routes
+  Sammy(function() {
+    this.get("#:folder", function() {
+      self.chosenFolderId(this.params.folder);
+      self.chosenMailData(null);
+      $.get("/mail", { folder: this.params.folder }, self.chosenFolderData);
+    });
+
+    this.get("#:folder/:mailId", function() {
+      self.chosenFolderId(this.params.folder);
+      self.chosenFolderData(null);
+      $.get("/mail", { mailId: this.params.mailId }, self.chosenMailData);
+    });
+
+    this.get("", function() {
+      this.app.runRoute("get", "#Inbox");
+    });
+  }).run();
 }
 
 ko.applyBindings(new WebmailViewModel());
