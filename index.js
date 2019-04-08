c39da6409a15b92217ce7331dfe6c9fdc3ceb303
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2019-04-08 16:44:46 -0400

    show inbox by default with data

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ function WebmailViewModel() {
   let self = this;
   self.folders = ["Inbox", "Archive", "Sent", "Spam"];
   self.chosenFolderId = ko.observable();
+  self.chosenFolderData = ko.observable();
 
   // Behaviours
   self.goToFolder = folder => {
     self.chosenFolderId(folder);
+    $.get(
+      "http://learn.knockoutjs.com/mail",
+      { folder: folder },
+      self.chosenFolderData
+    );
   };
+
+  // Show inbox by default
+  self.goToFolder("Inbox");
 }
 
 ko.applyBindings(new WebmailViewModel());
